1f56be6cdb90fa6c7ff04a739eadfecd21ed8204
Ammar Najjar <najjarammar@protonmail.com>
2019-06-12 13:50:31 +0200

    add parametrize tests example

--- test-demo.py
+++ test-demo.py
@@ -26,77 +26,19 @@ def tearDown(driver):
     driver.quit()
 
 
-@pytest.mark.usefixtures('setUp')
+@pytest.fixture(scope="function", params=[1, 2, 3, 4])
+def param(request):
+    yield request.param
+
+
+@pytest.mark.usefixtures('setUp', 'param')
 class TestCaseOne:
 
-    def test_1(self):
-        assert 1
+    def test_1(self, param):
+        assert 1 == param
 
-    def test_2(self):
-        assert 1
+    def test_2(self, param):
+        assert 2 == param
 
-    def test_3(self):
-        assert 1
-
-    def test_4(self):
-        assert 1
-
-    def test_5(self):
-        assert 1
-
-    def test_6(self):
-        assert 1
-
-    def test_7(self):
-        assert 1
-
-    def test_8(self):
-        assert 1
-
-    def test_9(self):
-        assert 1
-
-    def test_10(self):
-        assert 1
-
-    def test_11(self):
-        assert 1
-
-    def test_12(self):
-        assert 1
-
-    def test_13(self):
-        assert 1
-
-    def test_14(self):
-        assert 1
-
-    def test_15(self):
-        assert 1
-
-    def test_16(self):
-        assert 1
-
-    def test_17(self):
-        assert 1
-
-    def test_18(self):
-        assert 1
-
-    def test_19(self):
-        assert 1
-
-    def test_20(self):
-        assert 1
-
-    def test_21(self):
-        assert 1
-
-    def test_22(self):
-        assert 1
-
-    def test_23(self):
-        assert 1
-
-    def test_24(self):
-        assert 1
+    def test_3(self, param):
+        assert 3 == param
